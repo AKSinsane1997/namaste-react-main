08c3d1d5f6f59647bfdd3574cfbb955edf1deb39
AKSinsane1997 <81317739+AKSinsane1997@users.noreply.github.com>
2023-07-23 20:18:00 +0530

    first commit

---
+++ Food-Delivery-App/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <p data-testid="footer" className="text-center my-4">
+      @ Created by{" "}
+      
+        Akash Kumar Sadangi
+     
+    </p>
+  );
+};
+
+export default Footer;
